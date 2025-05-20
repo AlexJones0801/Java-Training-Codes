a4a281cb884976e8c93b61653aa26b7479bf20a4
AlexJones0801 <shivanshkurmi2@gmail.com>
2025-05-20 20:03:24 +0530

    Day 21

---
+++ Day 21 20_05_25/PracticeQCycle.java
@@ -0,0 +1,21 @@
+// Q7. Cyclically rotate an array by one
+
+public class PracticeQCycle {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,6,8,12};
+        int lastelem = arr[arr.length-1];
+
+
+        for(int i = arr.length-1 ; i > 0;i--) {
+                arr[i] = arr[i-1];
+        }
+        
+        arr[0] = lastelem;
+        
+        System.out.println("One Cyclical rotate array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
